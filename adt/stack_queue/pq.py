c88c9b54cd5005185614c18397414a8311cde48f
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-11-13 02:56:27 -0500

    Fix de enqueue method in PriorityQueueNodes class
    It was add a documentation in the PriorityQueueNodes class and it has fixed the enqueue method
    
    The file 'test_pq.py' it was extended to see the behavior for each PriorityQueue

--- adt/stack_queue/pq.py
+++ adt/stack_queue/pq.py
@@ -19,25 +19,24 @@ class PriorityQueueLong:
     def is_empty(self) -> bool:
         return self.queues.is_empty() and self.priorities.is_empty()
 
-    def enqueue(self, priority: int, data: object) -> bool:
+    def enqueue(self, data: object, priority: int) -> bool:
         """
-        Método que adiciona un nuevo Nodo con su dato
+        Método que adiciona un nuevo Nodo con su dato a la Queue
         correspondiente, según la prioridad que éste tendrá.
         priority → [1 > 2 > 3 > ... > n]
         """
-        if priority >= 1:
+        if type(priority) == int and priority > 0:
             if self.is_empty():
                 new_queue = Queue()
                 new_queue.enqueue(data)
-                if type(priority) == int:
-                    return self.priorities.append(
-                        priority
-                    ) and self.queues.append(new_queue)
+                return self.priorities.append(priority) and self.queues.append(
+                    new_queue
+                )
             elif type(data) == type(self.front()):
-                return self.__enqueue(priority, data)
+                return self.__enqueue(data, priority)
         return False
 
-    def __enqueue(self, priority: int, data: object) -> bool:
+    def __enqueue(self, data: object, priority: int) -> bool:
         new_queue = Queue()
         new_queue.enqueue(data)
         size = len(self.priorities)
@@ -55,9 +54,9 @@ class PriorityQueueLong:
         else:
             for i in range(len(self.priorities)):
                 if priority < self.priorities.locate(i):
-                    return self.priorities.insert(
-                        priority, i
-                    ) and self.queues.insert(new_queue, i)
+                    return self.priorities.insert(priority, i) and self.queues.insert(
+                        new_queue, i
+                    )
 
     def dequeue(self) -> object:
         if not self.is_empty():
@@ -69,7 +68,7 @@ class PriorityQueueLong:
 
     def front(self) -> object:
         if not self.is_empty():
-            return self.queues.locate(0).front
+            return self.queues.locate(0).front()
 
     def __len__(self) -> int:
         cnt = 0
@@ -100,9 +99,9 @@ class PriorityQueueShort:
     def is_empty(self) -> bool:
         return len(self) == 0
 
-    def enqueue(self, priority: int, data: object) -> bool:
+    def enqueue(self, data: object, priority: int) -> bool:
         """
-        Método que adiciona un nuevo Nodo con su dato
+        Método que adiciona un nuevo Nodo con su dato a la Queue
         correspondiente, según la prioridad que éste tendrá.
         priority → [1 > 2 > 3 > ... > n]
         """
@@ -116,8 +115,7 @@ class PriorityQueueShort:
                 else:
                     self.priorities.append(maximum + 1)
                     self.queues.append(Queue())
-                    return self.enqueue(priority, data)
-
+                    return self.enqueue(data, priority)
         return False
 
     def dequeue(self) -> object:
@@ -128,7 +126,7 @@ class PriorityQueueShort:
     def front(self) -> object:
         for i in self.queues:
             if not i.is_empty():
-                return i.front
+                return i.front()
 
     def __len__(self) -> int:
         cnt = 0
@@ -144,45 +142,58 @@ class PriorityQueueShort:
 
 
 class PriorityQueueNodes(Queue):
+    """
+    Clase que implementa el funcionamiento del ADT Priority
+    Queue, utilizando nodos del tipo PriorityNode. Serán
+    atendidos primeramente, o tienen MAYOR prioridad, los
+    nodos que por el contrario poseen un valor de prioridad
+    menor.
+    ATENCIÓN: Esta clase debe soportar el manejo de los
+    mismos métodos que implementa una clase Queue, con la
+    excepción del método enqueue que posee una
+    implementación diferente.
+    """
+
     def enqueue(self, new_data: object, priority: int = 1) -> bool:
-        if priority >= 1:
+        """
+        Método que adiciona un nuevo PriorityNode con su dato a la
+        PriorityQueue, según la prioridad que éste tendrá. A mayor valor de
+        la prioridad el nuevo PriorityNode se ubicará hacia el final de la
+        PriorityQueue.
+        - Si el nuevo PriorityNode tiene la misma prioridad que uno o varios
+        nodos de la PriorityQueue, el nuevo PriorityNode se ubicará
+        después del último PriorityNode con la misma prioridad.
+        priority → [1 > 2 > 3 > ... > n]
+        """
+        if type(priority) == int and priority >= 1:
             new_node = PriorityNode(new_data, priority)
             if self.is_empty():
                 self._front = self.tail = new_node
                 return True
             elif type(new_data) == type(self.front()):
-                return self.__enqueue(new_data, priority)
+                self.__enqueue(new_data, priority)
+                return True
         return False
 
-    def __enqueue(self, new_data, priority):
-        new_node = PriorityNode(new_data, priority)
-
+    def __enqueue(self, new_data: object, priority: int) -> None:
         current_node = self._front
+        cnt = 0
         while current_node:
-
-            if current_node.priority == priority:
-                current_node = self._front
-                while current_node:
-
-                    if priority < current_node.priority:
-                        current_node = current_node.next
-                    else:
-                        current_node.next, new_node.next = (
-                            new_node,
-                            current_node.next,
-                        )
-                        print('entre')
-                        return True
-
-            elif current_node.priority < priority:
-                current_node.next, new_node.next = (
-                    new_node,
-                    current_node.next,
-                )
-                return True
+            if priority < current_node.priority:
+                break
             else:
                 current_node = current_node.next
+                cnt += 1
 
-        print('fin')
-        self.tail.next = self.tail = new_node
-        return True
+        new_node = PriorityNode(new_data, priority)
+        if cnt == 0:
+            new_node.next = self._front
+            self._front = new_node
+        elif 0 < cnt < len(self):
+            current_node = self._front
+            for i in range(cnt - 1):
+                current_node = current_node.next
+            current_node.next, new_node.next = new_node, current_node.next
+        elif cnt == len(self):
+            self.tail.next = new_node
+            self.tail = new_node
--- test_pq.py
+++ test_pq.py
@@ -7,16 +7,46 @@ if __name__ == "__main__":
     pq = PriorityQueue()
 
     print("is_empty:", pq.is_empty())
+    print(f"dequeue: [is_empty: {pq.is_empty()}, front: {pq.front()}]")
+    print(pq.dequeue())
+    print(pq)
+
+    print(f"enqueue d{4} p{'3'} ")
+    print(f"len: {len(pq)}")
+    print(f"front: {pq.front()}")
+    print("enqueued", pq.enqueue(4, "3"))
+    print(pq)
 
     for i in range(1, 10):
         index = random.randint(1, 5)
-        print(f"enqueue p{index} d{i}")
+        print(f"enqueue d{i} p{index} ")
         print(f"len: {len(pq)}")
         print(f"front: {pq.front()}")
-        print('enqueued', pq.enqueue(i, index))
+        print("enqueued", pq.enqueue(i, index))
         print(pq)
 
     print("is_empty:", pq.is_empty())
+
+    cnt = 0
+    while not pq.is_empty() and cnt <= len(pq) / 2:
+        print(f"dequeue: [is_empty: {pq.is_empty()}, front: {pq.front()}]")
+        print(pq.dequeue())
+        print(pq)
+        cnt += 1
+
+    print(f"enqueue d{4} p{'3'} ")
+    print(f"len: {len(pq)}")
+    print(f"front: {pq.front()}")
+    print("enqueued", pq.enqueue(4, "3"))
+    print(pq)
+
+    for i in range(10, 20):
+        index = random.randint(1, 6)
+        print(f"enqueue d{i} p{index} ")
+        print(f"len: {len(pq)}")
+        print(f"front: {pq.front()}")
+        print("enqueued", pq.enqueue(i, index))
+        print(pq)
 
     while not pq.is_empty():
         print(f"dequeue: [is_empty: {pq.is_empty()}, front: {pq.front()}]")
