0268e9c9e709289a6ab251608c53c7ce2410be2f
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-11-13 03:46:00 -0500

    Fix the assignation to a error in the next property in SinglyLinkedNode class

--- adt/lists/nodes.py
+++ adt/lists/nodes.py
@@ -1,10 +1,10 @@
 class SinglyLinkedNode:
-    def __init__(self, data):
-        self.data = data  # almacena los datos para el nodo
-        self.next = None  # almacena la referencia al siguiente nodo
+    def __init__(self, data: object) -> None:
+        self.data = data
+        self.next = None
 
 
 class DoubleLinkedNode(SinglyLinkedNode):
-    def __init__(self, data):
+    def __init__(self, data: object) -> None:
         super().__init__(data)
-        self.previous = None  # almacena la referencia al nodo anterior
+        self.previous = None
